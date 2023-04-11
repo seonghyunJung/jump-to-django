78296637c95b396c11de31b6b384e01e742bdae9
seonghyunJung <tjdgusus@gmail.com>
2023-04-11 18:11:53 +0900

    초기 기능 구현
    페이징, 답변 개수 표시, 로그인과 로그아웃, 회원가입, 글쓴이 표시, CRUD, 추천, 마크다운, 검색

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from pybo.views import base_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("common/", include("common.urls")),
+    path("pybo/", include("pybo.urls")),
+    path("", base_views.index, name="index"),
 ]
